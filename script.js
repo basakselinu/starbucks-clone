11335b2ced8be316c2d655eff2766e0d55b14860
basakselinu <83473571+basakselinu@users.noreply.github.com>
2024-12-06 18:53:03 +0300

    added accordion, icons and footer

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ menu.addEventListener("click", () => {
     menuIcon.classList.add("fa-bars"); 
   }
 });
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
